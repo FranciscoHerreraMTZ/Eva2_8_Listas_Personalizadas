79f43801860fcd3adeb55c4ed702d2a64480565c
FranciscoHerreraMTZ <43562853+FranciscoHerreraMTZ@users.noreply.github.com>
2018-11-11 22:34:35 -0700

    Initial commit

---
+++ app/src/main/java/com/example/spart/eva2_8_listas_personalizadas/Clima.java
@@ -0,0 +1,13 @@
+package com.example.spart.eva2_8_listas_personalizadas;
+
+public class Clima {
+    String nombreCiudad,descripcion;
+    int temperatura,imagen;
+
+    public Clima(String nom,String des, int temp, int imag){
+        nombreCiudad = nom;
+        descripcion = des;
+        temperatura = temp;
+        imagen = imag;
+    }
+}
